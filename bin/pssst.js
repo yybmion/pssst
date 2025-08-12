b2ab583713e1c92f453057d1814ef568e7c130e7
yybmion <yunyubin54@gmail.com>
2025-08-12 22:29:57 +0900

    Test fetching yybmion repository messages

--- bin/pssst.js
+++ bin/pssst.js
@@ -5,12 +5,32 @@ const { program } = require('commander');
 const { getRandomMessage } = require('../lib/github');
 const chalk = require('chalk');
 
+function getTimeAgo(timestamp) {
+  const now = new Date();
+  const messageTime = new Date(timestamp);
+  const diffMs = now - messageTime;
+  const diffHours = Math.floor(diffMs / (1000 * 60 * 60));
+  const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
+
+  if (diffDays > 0) {
+    return `${diffDays}days before`;
+  } else if (diffHours > 0) {
+    return `${diffHours}hours before`;
+  } else {
+    return 'now';
+  }
+}
+
 program
 .action(async () => {
-  console.log('Fetching message ...');
   const message = await getRandomMessage();
-  console.log(chalk.cyan(`"${message}"`));
-  console.log(chalk.gray('- Bring message from Github'));
+
+  if (typeof message === 'string') {
+    console.log(chalk.red(message));
+  } else {
+    console.log(chalk.cyan(`"${message.text}"`));
+    console.log(chalk.gray(`- ${getTimeAgo(message.timestamp)}, author @${message.author} `));
+  }
 });
 
 program.parse();
